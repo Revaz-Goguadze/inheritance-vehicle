7d021371cbd280983b8041d06e3989c2dcc0429c
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2023-06-23 22:21:14 +0000

    Update Stub.cs

--- InheritanceVehicle/Stub.cs
+++ InheritanceVehicle/Stub.cs
@@ -1,5 +1,8 @@
-﻿namespace InheritanceVehicle
+namespace InheritanceVehicle
 {
+    /// <summary>
+    /// This class is only used in unit tests, DO NOT CHANGE it or ADD new functionality.
+    /// </summary>
     public sealed class Stub
     {
     }
